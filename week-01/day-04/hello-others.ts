e540f2dd866be81f7f22250308921763de09ccab
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-15 11:39:13 +0100

    done hello-others.ts

---
+++ week-01/day-04/hello-others.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+console.log('Hello, Eszter!');
+console.log('Hello, Sara');
+console.log('Hello, Dori!');
