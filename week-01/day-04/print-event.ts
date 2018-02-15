b992077a183dce2607c2df5267c2cf3e0420b930
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-15 17:33:13 +0100

    done print-event.ts

---
+++ week-01/day-04/print-event.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+let evenNums: number = 0;
+
+for(let i: number = 0; i <= 500; i++){
+    if (i % 2 === 0){
+        console.log(i);
+    } 
+}
+
