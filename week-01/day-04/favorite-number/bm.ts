fa33adefb6dce4b00e7f341de1de9b360e043df5
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-15 14:31:49 +0100

    delet wrong filename

--- week-01/day-04/favorite-number/bm.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let massInKg: number = 81.2;
-let heightInM: number = 1.78;
-
-// Print the Body mass index (BMI) based on these values
-
-let bmi: number = massInKg/((heightInM)*(heightInM));
-console.log("BMI = " + Math.round(bmi).toFixed(2));
