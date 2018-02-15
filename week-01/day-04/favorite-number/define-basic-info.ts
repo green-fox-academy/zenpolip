7c9a092002cf6826f7e200acaa312f43282e1ec5
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-15 14:51:05 +0100

    done define-basic-info.ts

---
+++ week-01/day-04/favorite-number/define-basic-info.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let myName: string = 'Balazs';
+let age: number = 30;
+let height: number = 1.80;
+let isMarried: boolean = false;
+
+console.log('My name is ' + myName +'. I`m ' +age +' years old. My height is ' +height +'m. Am I married? The answer is ' +isMarried +'.');
+
