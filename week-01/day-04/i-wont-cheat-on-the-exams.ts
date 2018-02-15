0f920e4a4640d9c6f2a01b3284b2b253c3347d0c
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-15 17:19:18 +0100

    done i-won-cheat-on-the-exams.ts

---
+++ week-01/day-04/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let oneLiner: string= "I won't cheat on the exam!";
+for (let i: number = 1; i <= 100; i++) {
+    console.log(i +' '+ oneLiner);
+}
