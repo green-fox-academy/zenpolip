9f20fa6bb42194cbc1af1d299f28f243ef718a0f
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-16 10:53:15 +0100

    done draw-triangle.ts

---
+++ week-01/day-04/draw-triangle.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+let lineCount: number = 4;
+
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+//for (let i: number = 0; i <
+let a: string = '*'; 
+for(lineCount; lineCount > 0; lineCount--){
+    console.log(a);
+    a= a + '*';
+    a
+}
