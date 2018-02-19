c77e01e996c812f366d60fffa3e09e930ecfcef1
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-19 17:24:47 +0100

    done 11-print-all.ts

---
+++ week-02/day1/11-print-all.ts
@@ -0,0 +1,9 @@
+// -  Create a variable named `numList` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `numList` to the console*
+// *hint: use a loop, console.log(numList) won't cut it
+// -  bonus for using the correct built in array method
+
+let numList: number[]= [4, 5, 6, 7];
+numList.forEach(function(element: number){
+  console.log(element);
+})
