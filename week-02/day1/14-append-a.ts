c494f97deabd466c28f5f26e75ef3a74ffe8eb4e
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-19 23:10:14 +0100

    add .map solution

--- week-02/day1/14-append-a.ts
+++ week-02/day1/14-append-a.ts
@@ -4,9 +4,16 @@
 
 let nimals: string[]= ['kuty', 'macsk', 'cic'];
 
-
-
+// using simple for loop
 for(let i: number= 0; i <= nimals.length-1; i++){
   nimals[i] += 'a';
 }
 console.log(nimals);
+
+/* using map function
+
+nimals = nimals.map(function(item: string){
+  return item + 'a';
+});
+console.log(nimals);
+*/
