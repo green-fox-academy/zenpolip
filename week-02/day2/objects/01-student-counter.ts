d22da7baf6fbf4b3bd5db424d897d41833c4e840
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-20 17:20:03 +0100

    extra added 01-student-counter.ts

--- week-02/day2/objects/01-student-counter.ts
+++ week-02/day2/objects/01-student-counter.ts
@@ -16,10 +16,50 @@ let students: any[] = [
 // - Sum of the age of people who have lass than 5 candies
 
 
+
 function candiesByName(studentsList: string[]){
-    for(let i: number= 0; i < students.length; i++){
-      console.log(students['name']);
+  console.log("Candies owned by each student:");
+  let sumOfCandies: number = 0;
+  for(let i: number= 0; i < students.length; i++){
+    console.log(students[i].name + ': ' +  students[i].candies);
+    sumOfCandies += students[i].candies;
   }
+  console.log("All the candies owned by the students: " + sumOfCandies);
 }
 
 candiesByName(students);
+
+
+/*  
+function for retrieving the number of candies by a student name
+
+function studentCandies(name: string) {
+  let filteredStudents: any[] = students.filter(function(student) {
+    return student.name === name;
+  });
+
+  // console.log(currentStudent[0].name + ': ' + currentStudent[0].candies)
+  filteredStudents.forEach(function(student) {
+    console.log(student.name + ': ' + student.candies)
+  })
+}
+
+studentCandies('Rezso');
+*/
+
+
+function studentsSum(candies) {
+  let filteredStudents: any[] = students.filter(function(student) {
+    return student.candies < 5;
+  });
+
+  let ageSum = 0;
+
+  filteredStudents.forEach(function(age) {
+    ageSum += age.age;
+  }); 
+  console.log("Sum of the age of people who have lass than 5 candies: " + ageSum);
+}
+
+studentsSum(5);
+
