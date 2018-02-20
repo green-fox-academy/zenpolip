b86ee2d1a4af93f7a076ccd6c1cccd294b68203c
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-20 12:27:48 +0100

    done 01-solar-system.ts

---
+++ week-02/day2/arrays/01-solar-system.ts
@@ -0,0 +1,16 @@
+'use strict';
+// Saturn is missing from the planetList
+// Insert it into the correct position
+// Create a function called putSaturn() which has list parameter and returns the correct list.
+// bonus for using some built in methods
+
+let planetList: string[] = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Uranus', 'Neptune'];
+// Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+
+function putSaturn(parameter: string[]): string[] {
+  planetList.push('Saturnus');
+  return planetList;
+}
+
+console.log(putSaturn(planetList));
+
