ab9a8ac1b4ccded9294450d7c1fb7153e8ba2eb1
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-02-27 11:44:08 +0100

    doublesave  checkerboard-ts.ts

--- week-02/day3/13-checkerboard/checkerboard-ts.ts
+++ week-02/day3/13-checkerboard/checkerboard-ts.ts
@@ -4,9 +4,25 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
-
 // Fill the canvas with a checkerboard pattern.
 
+let w: number = canvas.width / 8;
+let h: number = canvas.height / 8;
+let maxJ: number = Math.floor(w);
+let maxI: number = Math.floor(h);
+
+// draw squares in two lines at once
+function drawSquare(x: number, y: number, o: number, p: number) {
+  ctx.fillRect(x, y, w, h);
+  ctx.fillRect(x + w, y + h, w, h);
+}
+
+//  10 x 10 checkerboard every size of canvas with boxes
+for (let i: number = 0; i <= maxI; i++) {
+  for (let j: number = 0; j <= maxJ; j++) {
+    drawSquare(2 * i * w, 2 * j * h, w, h);
+  }
+}
 
 // let size: number = 120;
 // let s: number = 60;
@@ -27,24 +43,3 @@ const ctx = canvas.getContext('2d');
 //   ctx.fillRect(x,y,s,s);
 //   ctx.fillRect(x + s, y + s, s, s);
 // }
-
-
-let w: number = canvas.width / 10;
-let h: number = canvas.height / 10;
-let maxJ: number = Math.floor(canvas.width / 10);
-let maxI: number = Math.floor(canvas.height / 10);
-
-// draw squares in two lines at once
-function drawSquare(x: number, y: number, o: number, p: number) {
-  ctx.fillRect(x, y, w, h);
-  ctx.fillRect(x + w, y + h, w, h);
-}
-
-// checkerboard every size of canvas 10 x 10 with boxes
-for (let i: number = 0; i <= maxI; i++) {
-  for (let j: number = 0; j <= maxJ; j++) {
-    drawSquare(2 * i * w, 2 * j * h, w, h);
-  }
-}
-
-
