261c401394dd1189df7f6dcc486bdc1079595a2a
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-03-01 16:05:01 +0100

    corrected solution => 13-increment-element.ts

--- week-02/day1/13-increment-element.ts
+++ week-02/day1/13-increment-element.ts
@@ -1,7 +1,9 @@
 // -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
 // -  Increment the third element simply by accessing it
 // -  Log the third element to the console
+'use strict'
 
-let numList: number[]= [1, 2, 3, 4, 5];
-
-console.log(numList[2]+1);
+let numList: number[] = [1, 2, 3, 4, 5];
+console.log('3rd element of array: ' + numList[2]);
+numList[2]++;
+console.log('After incrementing the 3rd element: ' + numList[2]);
