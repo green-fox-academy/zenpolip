eb71f65236352088b31a37a0a85dd6a31205df91
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-03-01 13:10:38 +0100

    styleguide => blogpost.ts

--- week-03/day3/oop/blogpost.ts
+++ week-03/day3/oop/blogpost.ts
@@ -1,12 +1,12 @@
 'use strict'
 
 class BlogPost {
-  authorName:  string;
+  authorName: string;
   title: string;
   text: string;
   publicationDate: string;
 
-  constructor(authorName, title, text, publicationDate){
+  constructor(authorName, title, text, publicationDate) {
     this.authorName = authorName;
     this.title = title;
     this.text = text;
@@ -14,9 +14,9 @@ class BlogPost {
   }
 
   post() {
-    console.log(this.title + ' titled by ' + this.authorName + ' posted at ' + this.publicationDate + '\n' +this.text);
+    console.log(this.title + ' titled by ' + this.authorName + ' posted at ' + this.publicationDate + '\n' + this.text);
   }
 }
 
-let blogPost1 = new BlogPost('John Doe','Lorem Ipsum','Lorem ipsum dolor sit amet.','2000.05.04.');
+let blogPost1 = new BlogPost('John Doe', 'Lorem Ipsum', 'Lorem ipsum dolor sit amet.', '2000.05.04.');
 blogPost1.post()
