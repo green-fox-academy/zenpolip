261809a32b48b49c785e7cfe056f38005fd61e33
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-04-03 14:16:26 +0200

    reduced code

--- week-07/day1/frontend/index.js
+++ week-07/day1/frontend/index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
 const PORT = 3000;
-
-app.use('/static', express.static('static'));
-app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
  res.sendFile(path.join(__dirname, './index.html'));
