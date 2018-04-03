f9648fe0544c60e97d24d542cfb81575f935d3f6
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-04-03 15:03:26 +0200

    done doubling task

--- week-07/day1/frontend/index.js
+++ week-07/day1/frontend/index.js
@@ -4,9 +4,29 @@ const path = require('path');
 const PORT = 3000;
 
 app.get('/', (req, res) => {
- res.sendFile(path.join(__dirname, './index.html'));
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.use('/assets', express.static('assets'));
+
+
+app.get('/doubling', (req, res) => {
+  if (req.query.input === undefined) {
+    res.json({
+      "error": "Please provide an input!"
+    })
+  } else {
+    res.json({
+      "received": req.query.input,
+      "result": req.query.input * 2,
+    });
+  }
+});
+
+app.get('/greeter', (req, res) => {
+
 });
 
 app.listen(PORT, () => {
- console.log(`Server is up on port ${PORT}`);
+  console.log(`Server is up on port ${PORT}`);
 });
