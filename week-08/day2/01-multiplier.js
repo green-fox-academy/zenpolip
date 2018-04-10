098321b6d94c6e8b753191e625ed20fb7fd318d7
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-04-10 11:34:37 +0200

    done 01-multiplier.js

---
+++ week-08/day2/01-multiplier.js
@@ -0,0 +1,18 @@
+// Create the multiplier function that you can use like this:
+
+function multiplier(number) {
+  const multi = function(number2) {
+    return number * number2;
+  }
+  return multi;
+}
+
+const duplicator = multiplier(2);
+
+console.log(duplicator(5)); // should log 10
+console.log(duplicator(10)); // should log 20
+
+const threeTimes = multiplier(3);
+
+console.log(threeTimes(5)); // should log 15
+console.log(threeTimes(100)); // should log 300
