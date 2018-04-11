ec4e4c43b482e1b2072651622bd3d72a00d0cafe
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-04-11 18:57:25 +0200

    Added template engine

--- week-08/practice/src/index.js
+++ week-08/practice/src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index', {
+    title: 'Dashboard',
+  });
 });
 
 app.listen(PORT, () => {
