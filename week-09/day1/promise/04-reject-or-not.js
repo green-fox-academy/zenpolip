6a26274b3e2eb0dc6c45a1004a6ec4be9c58b88c
Balazs Csukonyi <balazscsukonyi@gmail.com>
2018-04-16 15:06:56 +0200

    done 04-reject-or-not.js

---
+++ week-09/day1/promise/04-reject-or-not.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var promise = new Promise(function (fulfill, reject) {
+  // Your solution here
+  fulfill('I FIRED');
+  reject(Error('I DID NOT FIRE'));
+});
+
+function onReject(error) {
+  // Your solution here
+  console.log(error.message);
+}
+
+// Your solution here
+
+promise.then(console.log, onReject);
